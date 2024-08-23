1bd1f5557368bea964e29c274978d9d46670bf3c
CMON1975 <c@cmon1975.com>
2024-08-23 08:13:01 -0700

    created .gitignore, corrected go mod init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CMON1975/bootdev-chirpy.git
+
+go 1.23.0
--- main.go
+++ main.go
@@ -1,31 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	// Create a new ServeMux
+	const port = "8080"
+
 	mux := http.NewServeMux()
 
-	// Register handlers
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Welcome to the home page!")
-	})
-
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World!")
-	})
-
-	// Create a new http.Server
-	server := &http.Server{
-		Addr:    ":8080", // Set the address to listen on port 8080
-		Handler: mux,     // Use the created ServeMux as the handler
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
 
-	// Start the server
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Println("Server failed:", err)
-	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
